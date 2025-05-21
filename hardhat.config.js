c418658d8dda0c455500a32f0067c7304a21fcca
Uzair Saiyed <justuzairsaiyed@gmail.com>
2025-05-21 14:29:43 +0530

    uniswap v3 deployer buildbear

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
+require("uniswap-v3-deploy-plugin");
+require("hardhat-deploy");
+require("@tokamak-network/tokamak-uniswap-v3-deploy");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.7.3",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    buildbear: {
+      url: process.env.BUILDBEAR_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
